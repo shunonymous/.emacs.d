a5cfdfa52032860186eb86fae75e60224382e08e
Shun Terabayashi <shunonymous@gmail.com>
2017-11-27 23:48:35 +0900

    hippie-expand

--- inits/00_misc.el
+++ inits/00_misc.el
@@ -42,3 +42,10 @@
 ;;; Store recently opened files
 (recentf-mode 1)
 
+;;; hippie-expand
+(setq hippie-expand-try-functions-list
+      '(try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill))
