fbaa4f5a99a57f11f0c869345fb59d6ae47cd6d1
Shun Terabayashi <shunonymous@gmail.com>
2017-12-10 22:01:34 +0900

    More tuning company-mode for C++

--- inits/20_auto-completion.el
+++ inits/20_auto-completion.el
@@ -1,5 +1,6 @@
 ;;; Settings for company-mode
-(require 'company)
+;(require 'company)
+;(add-hook 'after-init-hook 'global-company-mode)
 (global-company-mode 1)
 
 ;;; No auto popup
@@ -25,30 +26,19 @@
                     :background "gray40")
 
 ;;; Keybinds
-(global-set-key (kbd "<tab>") 'company-complete)
-(define-key company-active-map (kbd "<tab>") 'company-select-next)
+;;; Wrapper for tab key
+;;; If completion candidate is exists, launch company-completion.
+;;; Otherwise, indent.
+(define-key company-mode-map (kbd "<tab>") 'company-indent-or-complete-common)
 
-(defun company--insert-candidate2 (candidate)
-  (when (> (length candidate) 0)
-    (setq candidate (substring-no-properties candidate))
-    (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
-        (insert (company-strip-prefix candidate))
-      (if (equal company-prefix candidate)
-          (company-select-next)
-          (delete-region (- (point) (length company-prefix)) (point))
-        (insert candidate))
-      )))
+(define-key company-active-map "<tab>" 'company-select-next-if-tooltip-visible-or-complete-selection)
+(define-key company-active-map "<backtab>" 'company-select-previous)
 
-(defun company-complete-common2 ()
-  (interactive)
-  (when (company-manual-begin)
-    (if (and (not (cdr company-candidates))
-             (equal company-common (car company-candidates)))
-        (company-complete-selection)
-      (company--insert-candidate2 company-common))))
-
-(define-key company-active-map [tab] 'company-complete-common2)
-(define-key company-active-map [backtab] 'company-select-previous)
+;;; Backend for C++, using irony.
+(require 'irony)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-to-list 'company-backends 'company-irony)
 
 ;;;; Settings for auto-complete-mode
 ;(require 'auto-complete)
