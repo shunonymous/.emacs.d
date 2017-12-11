5371db2b88778daddba7c4c0e222795e650cf7e8
Shun Terabayashi <shunonymous@gmail.com>
2017-12-12 00:05:27 +0900

    Sync auto generated code

--- init.el
+++ init.el
@@ -16,7 +16,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (init-loader))))
+ '(package-selected-packages
+   (quote
+    (flycheck-rtags flycheck-irony irony-eldoc rtags company-shell company company-c-headers company-emoji company-irony company-irony-c-headers flycheck-popup-tip flycheck flycheck-clang-analyzer cpputils-cmake cmake-font-lock cmake-ide cmake-mode cmake-project minibuf-isearch sudo-edit ac-clang ac-emoji ac-helm ac-mozc auto-complete cherry-blossom-theme magit mozc mozc-im mozc-popup mozc-temp pkgbuild-mode projectile rainbow-delimiters undo-tree volatile-highlights init-loader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
