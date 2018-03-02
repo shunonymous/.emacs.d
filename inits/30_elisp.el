1cece926d2eeab53d41f69102778ae8ac60bf68c
Shun Terabayashi <shunonymous@gmail.com>
2018-03-02 18:32:05 +0900

    Add settings for Emacs-lisp

---
+++ inits/30_elisp.el
@@ -0,0 +1,13 @@
+(require 'open-junk-file)
+(global-set-key (kbd "C-x C-z") 'open-junk-file)
+
+(require 'lispxmp)
+(define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp)
+
+(require 'auto-async-byte-compile)
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(setq eldoc-idle-delay 0.2)
