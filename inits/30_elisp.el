7b42be6720f253b91522f75755fdf80c1a0e26a2
Shun Terabayashi <shunonymous@gmail.com>
2018-10-05 23:38:59 +0900

    Add .emacs.d to exclude auto byte compile elisp

--- inits/30_elisp.el
+++ inits/30_elisp.el
@@ -5,7 +5,7 @@
 (define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp)
 
 (require 'auto-async-byte-compile)
-(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/\\|/.emacs.d/")
 (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
