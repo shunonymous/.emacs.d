d1b06be26e763e2d503f5ff2469df619ddac4dad
Shun Terabayashi <shunonymous@gmail.com>
2018-11-24 17:54:48 +0900

    Revert "Refresh package list in background when launching"
    This reverts commit 1c88ffc18b198ac178408fab5fcb1961afdb5f83.

--- init.el
+++ init.el
@@ -7,9 +7,7 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
-
-(make-thread 'package-refresh-contents)
+(package-initialize) ;; You might already have this line
 
 (require 'init-loader)
 (init-loader-load)
