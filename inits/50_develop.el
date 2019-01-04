aff8b5c1c49af8634db958557fcf0ddc94388058
Shun Terabayashi <shunonymous@gmail.com>
2019-01-04 16:43:53 +0900

    Introduce dashboard

--- inits/50_develop.el
+++ inits/50_develop.el
@@ -1,5 +1,14 @@
 ; Settings for programming, mainly C++
 
+;; dashboard
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
+
 ;; If C++ mode, set indent mode as stroustrup
 (add-hook 'c++-mode-hook
 	  '(lambda()
