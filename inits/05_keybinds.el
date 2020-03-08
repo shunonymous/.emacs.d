b7153ced21577f596645b2a391f847ad5e90ab9e
Shun Terabayashi <shunonymous@gmail.com>
2020-03-09 00:33:45 +0900

    Avoid key conflict

--- inits/05_keybinds.el
+++ inits/05_keybinds.el
@@ -1,9 +1,6 @@
 ;;; Moving buffer by M-<Arrow-key>
-(global-set-key (kbd "M-<up>") 'windmove-up)
-(global-set-key (kbd "M-<down>") 'windmove-down)
-(global-set-key (kbd "M-<left>") 'windmove-left)
-(global-set-key (kbd "M-<right>") 'windmove-right)
+(windmove-default-keybindings)
 
 ;;; Switch between frame
-(global-set-key (kbd "M-RET") 'other-frame)
+(global-set-key (kbd "<S-return>") 'other-frame)
 
